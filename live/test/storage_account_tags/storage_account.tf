9511cadbf3c73b266a99ac8e8b5872b73c37b652
Ricardo Tolentino <ricardo.tolentino@rackspace.com>
2024-06-28 15:22:05 -0500

    Terrafrom apply only if plan has changes

--- live/test/storage_account_tags/storage_account.tf
+++ live/test/storage_account_tags/storage_account.tf
@@ -12,10 +12,13 @@ resource "azurerm_storage_account" "sa" {
   count = local.storage_account_count
 
   #name                = "${var.storage_account_prefix}${format("%04d", random_integer.random_storage_account_int.id)}"
-  name                            = "${local.storage_account_name_prefix}${format("%04d", random_integer.random_storage_account_int[count.index].id)}"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = azurerm_resource_group.rg.location
+  name                = "${local.storage_account_name_prefix}${format("%04d", random_integer.random_storage_account_int[count.index].id)}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags                = {}
+
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = true
+
 }
