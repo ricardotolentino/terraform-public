4cb346342724147d2c9194f6dd6a2bf6dfd00a34
Ricardo Tolentino <ricardo.tolentino@rackspace.com>
2024-07-10 12:40:08 -0500

    Update TF reusable workflow

--- live/test/storage_account_tags/storage_account.tf
+++ live/test/storage_account_tags/storage_account.tf
@@ -16,7 +16,7 @@ resource "azurerm_storage_account" "sa" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   tags = {
-    foo = "test"
+    foo = "02"
   }
 
   account_tier                    = "Standard"
